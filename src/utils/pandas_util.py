36c051b9b945ef8c60e3778851f7bf7f38a8f074
Gupta <mngup@8c859021d45b.ant.amazon.com>
2019-01-10 08:41:55 +0530

    added test train split method

--- src/utils/pandas_util.py
+++ src/utils/pandas_util.py
@@ -1,5 +1,18 @@
 import pandas as pd
-import tarfile
+from sklearn.model_selection import train_test_split
 
 def load_data_frame_from_csv_file(path):
     return pd.read_csv(path)
+
+def get_training_data_label_by_label_columns(data_frame, label_columns):
+    y = data_frame[label_columns]
+    X = data_frame.drop(columns=label_columns)
+    return X, y
+
+
+def split_data_frame_train_test(X, y, test_size=0.25):
+    return train_test_split(X, y, test_size=test_size)
+
+
+
+
