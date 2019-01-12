ff0f1d339f4ec63482cf474c9ac0a5e5d7a0d803
Gupta <mngup@8c859021d45b.ant.amazon.com>
2019-01-12 11:26:28 +0530

    added stratified sampling in train test for a given dataframe

--- src/utils/pandas_util.py
+++ src/utils/pandas_util.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 
 def load_data_frame_from_csv_file(path):
     return pd.read_csv(path)
@@ -10,8 +10,19 @@ def get_training_data_label_by_label_columns(data_frame, label_columns):
     return X, y
 
 
-def split_data_frame_train_test(X, y, test_size=0.25):
-    return train_test_split(X, y, test_size=test_size)
+def random_split_data_frame_train_test_label(X, y, test_size=0.25, random_state=42):
+    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+def stratified_shuffle_split_data_frame_train_test(X, y, test_size=0.25, n_splits=1, random_state=42):
+    stratified_shuffle_split = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=random_state)
+
+    for train_index, test_index in stratified_shuffle_split.split(X, y):
+        X_train = X.loc[train_index]
+        X_test = X.loc[test_index]
+    return X_train, X_test
+
+
+
 
 
 
