8aced427b632d3eaed3ee7eb61eb64999572e797
Gupta <mngup@8c859021d45b.ant.amazon.com>
2019-02-03 00:11:06 +0530

    fixed OrderedCategoricalDataEncoder ability to take mmultiple column dataframe by iterating over all columns and then appending

--- src/custom_transformers/OrderedCategoricalDataEncoder.py
+++ src/custom_transformers/OrderedCategoricalDataEncoder.py
@@ -19,5 +19,10 @@ class OrderedCategoricalDataEncoder(CustomPipelineTransformer):
 
     def transform(self, data_frame):
         if isinstance(data_frame, pd.DataFrame):
-            return pd.DataFrame(self.pipeline.fit_transform(data_frame.values))
+            res_data_frame = pd.DataFrame()
+            for column in data_frame:
+                temp_data_frame = data_frame[[column]]
+                temp_data_frame = pd.DataFrame(self.pipeline.fit_transform(temp_data_frame.values.ravel()))
+                res_data_frame = pd.concat([res_data_frame, temp_data_frame], axis=1)
+            return res_data_frame
         raise ValueError("first parameter should be of type pandas DataFrame")
